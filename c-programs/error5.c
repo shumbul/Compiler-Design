dfe4ecb934caa71f6626d49ad0e14de6e28abec2
jahnavikesana <69144271+jahnavikesana@users.noreply.github.com>
2020-08-22 12:54:08 +0530

    Update error5.c

--- c-programs/error5.c
+++ c-programs/error5.c
@@ -1,6 +1,5 @@
 /*In the second statement, myinteger is a data type, and the lexer should return myinteger as a datatype, not as an indentifier.*/
 /*error i identifying the type of token*/
-#include<stdio.h>
 typedef int myinteger; 
 myinteger x;
 int main()
