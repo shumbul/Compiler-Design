aa120a3806cc99089f2ce416f6894dc2668e73bc
shumbul <arifas18922@gmail.com>
2020-08-26 23:52:01 +0530

    removing extra folder

--- c-programs/error5.c
+++
@@ -1,8 +0,0 @@
-/*In the second statement, myinteger is a data type, and the lexer should return myinteger as a datatype, not as an indentifier.*/
-/*error i identifying the type of token*/
-typedef int myinteger; 
-myinteger x;
-int main()
-{
-    x=1.543;
-}
