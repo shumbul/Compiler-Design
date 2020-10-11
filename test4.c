4ccaf05d977eb7df67f9a28969f0aade89b9e8b3
Keerti Chaudhary <43823232+keerti2001@users.noreply.github.com>
2020-10-11 09:59:34 +0530

    added syntax analyser code

---
+++ test4.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+struct st{
+	int a;
+};
+
+void main()
+{
+ 	char s[30];
+ 	s= "nitk surathkal";
+ 	printf("\nString is:",s);
+ 	return;
+}
---
+++ test5.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+void main()
+{
+ 	char s[30];
+ 	s= "nitk surathkal;
+ 	printf("\nString is:",s);
+ 	return;
+}
